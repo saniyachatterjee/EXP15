bc8b18052c6f6020aa4ebe4be79e6b3e171ec195
saniyachatterjee <saniyaa0103@gmail.com>
2024-09-30 12:20:49 +0530

    Create 15_1.cpp

---
+++ 15_1.cpp
@@ -0,0 +1,23 @@
+//Saniya Chatterjee
+//PRN 23070123113
+//EnTC B2
+
+#include <iostream>
+using namespace std;
+
+int fact(int n) {
+    if (n <= 1) {
+        return 1;
+    } else {
+        return (n * fact(n - 1));
+    }
+}
+
+int main() {
+    int X, n;
+    cout << "Enter a number: ";
+    cin >> n;
+    X = fact(n);
+    cout << "Factorial: " << X << endl;
+    return 0;
+}
